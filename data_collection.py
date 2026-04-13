b677700d45d7c03c35be6ac339761ea8b2116d9f
Whalien08 <mithraxavier2005@gmail.com>
2026-04-13 16:04:38 +0530

    data collection

--- data_collection.py
+++ data_collection.py
@@ -1,30 +1,56 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
+import math
+import time
 
 cap=cv2.VideoCapture(0)
 det=HandDetector(maxHands=1)
 offset=20
 imgSize=300
 
+folder="Data/B"
+counter=0
+
 while True:
     success, img = cap.read()
     hands, img = det.findHands(img)
     if hands:
-        hand =hands[0]
-        x,y,w,h = hand['bbox']
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        # Reset imgWhite every time a hand is detected so it's clean
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
 
         y1, y2 = max(0, y - offset), min(img.shape[0], y + h + offset)
         x1, x2 = max(0, x - offset), min(img.shape[1], x + w + offset)
         imgCrop = img[y1:y2, x1:x2]
-        if imgCrop.size>0:
-            imgWhite = np.ones((imgSize,imgSize,3),np.uint8)*255
-            h_crop, w_crop, _ = imgCrop.shape
-            h_final = min(h_crop, imgSize)
-            w_final = min(w_crop, imgSize)
 
-            imgWhite[0:h_final, 0:w_final] = imgCrop[:h_final, :w_final]
-        cv2.imshow("ImageCrop",imgCrop)
-        cv2.imshow("ImageWhite",imgWhite)
-    cv2.imshow("Image",img)
-    cv2.waitKey(1) 
+        if imgCrop.size > 0:
+            aspectRatio = h / w
+
+            if aspectRatio > 1:
+                # Case: Tall Hand (fix height to 300)
+                k = imgSize / h
+                wCal = math.ceil(k * w)
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                wGap = math.ceil((imgSize - wCal) / 2)
+                imgWhite[:, wGap:wCal + wGap] = imgResize
+            else:
+                # Case: Wide Hand (fix width to 300)
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize
+
+            cv2.imshow("ImageCrop", imgCrop)
+            cv2.imshow("ImageWhite", imgWhite)
+            
+    cv2.imshow("Image", img)
+    key= cv2.waitKey(1)
+
+    if key == ord("s"):
+        counter+=1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        print(counter)
